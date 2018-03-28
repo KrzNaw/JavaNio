ef6418a91b0e31ae8f4752f8d384876585253b8a
KrzNaw <krzysztof.nawrocki@poczta.onet.pl>
2018-03-28 10:40:40 +0200

    pipe and file copy added

--- src/java_nio/File_Channel.java
+++ src/java_nio/File_Channel.java
@@ -69,6 +69,16 @@ public class File_Channel {
 
             System.out.println("int1 = " + readBuffer.getInt());
 
+            RandomAccessFile copyFile = new RandomAccessFile("data.dat", "rw");
+            FileChannel copyChannel = copyFile.getChannel();
+//            long numTransferred = copyChannel.transferFrom(channel, 0, channel.size());
+            long numTransferred = channel.transferTo(0, channel.size(), copyChannel);
+            System.out.println("Num transferred " + numTransferred);
+
+            channel.close();
+            ra.close();
+            copyChannel.close();
+/*
             byte[] outputString = "Hello, World!".getBytes();
             long str1Pos = 0;
             long newInt1Pos = outputString.length;
@@ -99,6 +109,8 @@ public class File_Channel {
             binChannel.write(ByteBuffer.wrap(outputString));
             binChannel.position(str2Pos);
             binChannel.write(ByteBuffer.wrap(outputString2));
+            */
+
   /*          ByteBuffer readBuffer = ByteBuffer.allocate(100);
             channel.read(readBuffer);
             readBuffer.flip();
---
+++ src/java_nio/Pipes.java
@@ -0,0 +1,74 @@
+package java_nio;
+
+import java.io.IOException;
+import java.nio.ByteBuffer;
+import java.nio.channels.Pipe;
+
+public class Pipes {
+    public static void main(String[] args) {
+
+        try{
+            Pipe pipe = Pipe.open();
+
+            Runnable writer = new Runnable() {
+                @Override
+                public void run() {
+                    try{
+                        Pipe.SinkChannel sinkChannel = pipe.sink();
+                        ByteBuffer buffer = ByteBuffer.allocate(56);
+
+                        for (int i =0; i<10; i++){
+                            String currentTime = "The time is: " + System.currentTimeMillis();
+
+                            buffer.put(currentTime.getBytes());
+                            buffer.flip();
+
+                            while (buffer.hasRemaining()){
+                                sinkChannel.write(buffer);
+                            }
+                            buffer.flip();
+                            Thread.sleep(100);
+
+                        }
+
+                    } catch (Exception e){
+                        System.out.println("Pipe exception: " + e.getMessage());
+                        e.printStackTrace();
+                    }
+
+                }
+            };
+
+            Runnable reader = new Runnable() {
+                @Override
+                public void run() {
+                    try{
+                        Pipe.SourceChannel sourceChannel = pipe.source();
+                        ByteBuffer buffer = ByteBuffer.allocate(56);
+
+                        for (int i = 0; i<10; i++){
+                            int bytesRead = sourceChannel.read(buffer);
+                            byte[] timeString = new byte[bytesRead];
+                            buffer.flip();
+                            buffer.get(timeString);
+                            System.out.println("Reader thread: " + new String (timeString));
+                            buffer.flip();
+                            Thread.sleep(100);
+                        }
+                    } catch (Exception e){
+                        e.printStackTrace();
+                    }
+
+                }
+            };
+
+            new Thread(writer).start();
+            new Thread(reader).start();
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+
+    }
+}
